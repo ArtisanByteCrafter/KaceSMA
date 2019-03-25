58b20f7a9d46ef18b7f10e5f067b364b8c48c802
Nathaniel Webb <natethegreat440@gmail.com>
2019-03-25 16:05:57 -0500

    for pester tests

---
+++ tests/functions.Tests.ps1
@@ -0,0 +1,67 @@
+$root = Split-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) -Parent
+
+$FunctionsToExport = @(
+    'Get-MachineInventory'
+    'Get-CurrentUserAccount'
+    'Get-UserPermissions'
+    'Get-AgentlessInventory'
+    'Get-OperatingSystemInventory'
+    'Get-MachineProcess'
+    'Get-MachineService'
+    'Get-SoftwareInventory'
+    'Get-StartupProgramInventory'
+    'Get-Asset'
+    'Get-AssetType'
+    'Get-BarcodeAsset'
+    'Get-ManagedInstall'
+    'Get-ManagedInstallMachineCompatibility'
+    'Get-ReportingDefinition'
+    'Get-ScriptRunStatus'
+    'Get-Script'
+    'Get-ScriptDependency'
+    'Get-ScriptTask'
+    'New-Asset'
+    'New-Script'
+    'New-ScriptTask'
+    'Invoke-Script'
+    'Get-ArchiveAsset'
+    'Set-AssetAsArchived'
+    'Get-ServiceDeskQueue'
+    'Get-ServiceDeskQueueField'
+    'Get-ServiceDeskTicketTemplate'
+    'Get-ServiceDeskTicket'
+    'New-ServiceDeskTicket'
+    'Set-MachineInventory'
+    'Set-ServiceDeskTicket'
+    'Remove-ServiceDeskTicket'
+    'Get-ServiceDeskTicketChanges'
+)
+
+Describe "General Function Tests" {
+    Foreach ($function in $FunctionsToExport) {
+        Context "$function has valid structure" {
+        
+        
+            It "PUBLIC function $function.ps1 should exist" {
+                "$root\public\*\$function.ps1" | Should -Exist
+            }
+            It "PUBLIC function $function.ps1 HELP block should exist" {
+                "$root\public\*\$function.ps1" | Should -FileContentMatch '<#'
+                "$root\public\*\$function.ps1" | Should -FileContentMatch '#>'
+                
+            }
+            It "PUBLIC function $function.ps1 should have Example" {
+                "$root\public\*\$function.ps1" | Should -FileContentMatch '.EXAMPLE'
+            }
+            It "PUBLIC function $function.ps1 should have Description" {
+                "$root\public\*\$function.ps1" | Should -FileContentMatch '.DESCRIPTION'
+            }
+            It "$function.ps1 is valid Powershell code" {
+                $psFile = Get-Content -Path "$root\public\*\$function.ps1" -ErrorAction Stop
+                $errors = $null
+                $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+                $errors.Count | Should Be 0
+            }
+        }
+    }
+}
