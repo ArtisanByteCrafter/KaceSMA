08cb3746cf1524f8de95bec26bed61a35a9a3001
Nathaniel Webb <natethegreat440@gmail.com>
2019-07-08 19:58:08 -0500

    no need to try/catch module import really.

--- KaceSMA.psm1
+++ KaceSMA.psm1
@@ -2,14 +2,4 @@
 $Public  = Get-ChildItem $PSScriptRoot\public\*.ps1 -Recurse -ErrorAction SilentlyContinue
 $Private = Get-ChildItem $PSScriptRoot\private\*.ps1 -Recurse -ErrorAction SilentlyContinue
 
-
-Foreach($import in @($Public + $Private)) {
-    Try {
-        . $import.fullname
-    }
-    Catch {
-        Write-Error "Failed to import function $($import.fullname)"
-    }
-}
-
-#Export-ModuleMember -function (Get-ChildItem -Path "$PSScriptRoot\public\*.ps1" -Recurse).basename
+Foreach($import in @($Public + $Private)) { . $import.fullname }
