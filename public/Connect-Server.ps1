8dfc42b71ab126bf1d5aaa17c060c97fd45f7e1a
Nathaniel Webb <natethegreat440@gmail.com>
2019-10-30 22:43:57 -0500

    added new connect-smaserver cmdlet

---
+++ public/Connect-Server.ps1
@@ -0,0 +1,77 @@
+Function Connect-Server {
+    param(
+        [Parameter(Mandatory)]
+        [ValidateScript( {
+                If ($_ -notmatch "^(http|https)://") {
+                    Throw 'Must start with "http://" or "https://"'
+                }
+                Else { $true } }
+        )]
+        [String]
+        $Server,
+
+        [Parameter(Mandatory)]
+        [String]
+        $Org,
+
+        [Parameter(Mandatory)]
+        [PSCredential]
+        $Credential
+
+    )
+
+    Begin {
+        $Auth = @{
+            'password'         = ($Credential.GetNetworkCredential().password)
+            'userName'         = ($Credential.username)
+            'organizationName' = $Org
+        } | ConvertTo-Json
+
+        $script:Server = $Server
+        $script:Org = $Org
+
+        # Dynamically find and include all available protocols 'Tls12' or higher.
+        # Module requires PS 5.1+ so no error checking should be required.
+
+        $CurrentVersionTls = [Net.ServicePointManager]::SecurityProtocol
+        Set-ClientTlsProtocols -ErrorAction Stop
+    
+        $Uri = "{0}/ams/shared/api/security/login" -f $script:Server
+
+        $script:Session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+
+        $script:Headers = @{ }
+        $script:Headers.Add('Accept', 'application/json')
+        $script:Headers.Add('Content-Type', 'application/json')
+        $script:Headers.Add('x-dell-api-version', '8')
+
+        $RequestSplat = @{
+            Uri             = $Uri
+            Headers         = $script:Headers
+            Body            = $Auth
+            Method          = 'POST'
+            WebSession      = $script:Session
+            UseBasicParsing = $True
+        }
+        $Request = Invoke-WebRequest @RequestSplat
+
+        $script:CSRFToken = $Request.Headers.'x-dell-csrf-token'
+        $script:Headers.Add("x-dell-csrf-token", "$script:CSRFToken")
+
+        # If we received a token, we're authenticated
+        If ($script:CSRFToken) {
+            [PSCustomObject]@{
+                Server = $script:Server
+                Status = 'Connected'
+                Token  = $script:CSRFToken
+            } | Format-List
+        }
+        Else {
+            Throw "A connection to '$script:Server' could not be established."
+        }
+    }
+    End {
+        # Be nice and set session security protocols back to how we found them.
+        [Net.ServicePointManager]::SecurityProtocol = $currentVersionTls
+    }
+}
