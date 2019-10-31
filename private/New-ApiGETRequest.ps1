bc173555afdc2768aca42f28ff6eb25c87711b6c
Nathaniel Webb <natethegreat440@gmail.com>
2019-10-30 23:01:20 -0500

    new auth method for New-APIGetRequest

--- private/New-ApiGETRequest.ps1
+++ private/New-ApiGETRequest.ps1
@@ -1,80 +1,33 @@
 Function New-ApiGETRequest {
     <#
     .DESCRIPTION
-        Performs an authentication request, then a GET request to the provided endpoint.
+        Performs an authenticated request to the specified endpoint with (optional) parameters
     #>
     [cmdletBinding()]
     param (
-        [Parameter(Mandatory)]
-        [ValidateScript({
-            If ($_ -notmatch "^(http|https)://") {
-                Throw 'Must start with "http://" or "https://"'
-        } Else{ $true }}
-        )]
-        [String]
-        $Server,
-
-        [Parameter(Mandatory)]
-        [String]
-        $Org,
 
         [Parameter(Mandatory)]
         [String]
         $Endpoint,
-
-        [Parameter(Mandatory)]
-        [PSCredential]
-        $Credential,
 
         [Parameter()]
         [String]
         $QueryParameters
     )
 
-    $Auth = @{
-        'password'         = ($Credential.GetNetworkCredential().password)
-        'userName'         = ($Credential.username)
-        'organizationName' = $Org
-    } | ConvertTo-Json
-
-
-    # Dynamically find and include all available protocols 'Tls12' or higher.
-    # Module requires PS 5.1+ so no error checking should be required.
+    If ($QueryParameters) {
+        $APIUrl = "{0}{1}{2}" -f $script:Server, $Endpoint, $QueryParameters
+    }
+    Else { $APIUrl = "{0}{1}" -f $script:Server, $Endpoint }
 
     $CurrentVersionTls = [Net.ServicePointManager]::SecurityProtocol
     Set-ClientTlsProtocols -ErrorAction Stop
 
-    $Uri = "$Server/ams/shared/api/security/login"
-    $Session = New-Object Microsoft.Powershell.Commands.Webrequestsession
-
-    $Headers = @{}
-    $headers.Add('Accept', 'application/json')
-    $headers.Add('Content-Type', 'application/json')
-    $headers.Add('x-dell-api-version', '8')
-
-    $RequestSplat = @{
-        Uri             = $Uri
-        Headers         = $Headers
-        Body            = $Auth
-        Method          = 'POST'
-        WebSession      = $Session
-        UseBasicParsing = $True
-    }
-    $Request = Invoke-WebRequest @RequestSplat
-
-    $CSRFToken = $Request.Headers.'x-dell-csrf-token'
-    $headers.Add("x-dell-csrf-token", "$CSRFToken")
-
-    If ($QueryParameters) {
-        $APIUrl = "{0}{1}{2}" -f $Server,$Endpoint,$QueryParameters
-    }
-    Else { $APIUrl = "{0}{1}" -f $Server,$Endpoint }
-
     $IRMSplat = @{
         Uri             = $APIUrl
-        Headers         = $Headers
+        Headers         = $script:Headers
         Method          = 'GET'
-        WebSession      = $Session
+        WebSession      = $script:Session
         UseBasicParsing = $True
     }
     Invoke-RestMethod @IRMSplat
